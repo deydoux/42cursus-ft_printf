6bff34a7abf7fe79c05613f426cfbd1eadcfdebd
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-11-17 20:09:22 +0100

    Update Libft

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:30:49 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/15 08:56:59 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/17 10:38:18 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	bytes;
 	void	*ptr;
 
-	if (!size && nmemb > __SIZE_MAX__ / size)
+	if (size && nmemb > __SIZE_MAX__ / size)
 		return (NULL);
 	bytes = nmemb * size;
 	ptr = malloc(bytes);
