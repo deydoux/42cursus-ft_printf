3f5c8ddbfed9929c57e54772c551336aebf832af
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-11-18 10:11:48 +0100

    Remove octal conversion

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:10:12 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/17 23:50:53 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/18 10:11:38 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,6 @@ static int	convert(const char **format, va_list *ap)
 	flags = get_flags(format, ap);
 	if (**format == 'd' || **format == 'i')
 		return (print_int(flags, ap));
-	else if (**format == 'o')
-	{
-
-	}
 	else if (**format == 'u')
 		return (print_unsigned(flags, ap, "0123456789", ""));
 	else if (**format == 'x')
