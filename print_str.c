0a48bf2432e8d270d41b416b20f88f47f8cb3b79
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-20 12:34:32 +0100

    Fix print_str: handle null str with flags

--- print_str.c
+++ print_str.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 15:56:36 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/20 08:17:20 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/20 12:30:11 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,30 @@ static int	update_flags(t_flags *flags, const char *s)
 	return (len);
 }
 
+static int	put_null(t_flags flags)
+{
+	int	len;
+
+	len = 6 * (flags.precision == -1);
+	flags.width -= len;
+	if (!flags.left_adjust)
+	{
+		while (flags.width-- > 0)
+		{
+			ft_putchar_fd(' ', 1);
+			len++;
+		}
+	}
+	if (flags.precision == -1)
+		write(1, "(null)", 6);
+	while (flags.width-- > 0)
+	{
+		ft_putchar_fd(' ', 1);
+		len++;
+	}
+	return (len);
+}
+
 int	print_str(va_list *ap, t_flags flags)
 {
 	const char	*s;
@@ -32,14 +56,13 @@ int	print_str(va_list *ap, t_flags flags)
 
 	s = va_arg(*ap, const char *);
 	if (!s)
-		s = "(null)";
+		return (put_null(flags));
 	len = update_flags(&flags, s);
 	if (!flags.left_adjust)
 		while (flags.width-- > 0)
-			ft_putchar_fd(flags.padding, 1);
-	write(1, s, flags.precision);
-	if (flags.left_adjust)
-		while (flags.width-- > 0)
 			ft_putchar_fd(' ', 1);
+	write(1, s, flags.precision);
+	while (flags.width-- > 0)
+		ft_putchar_fd(' ', 1);
 	return (len);
 }
