1117cd9cad9ad0e2b682a39193bdd56b716dd746
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-20 14:08:38 +0100

    Fix print_unsigned: handle null number with precision

--- print_unsigned.c
+++ print_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 20:11:12 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/20 12:29:22 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/20 14:07:47 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,15 @@ static int	update_flags(t_flags *flags, unsigned long long n, size_t base_len,
 
 	if (flags->precision != -1)
 		flags->padding = ' ';
-	len = !n && flags->precision && flags->precision != 1;
+	len = !n && flags->precision == -1;
 	tmp_n = n;
 	while (tmp_n)
 	{
 		tmp_n /= base_len;
 		len++;
 	}
-	flags->precision -= len;
+	if (n)
+		flags->precision -= len;
 	len += (flags->precision * (flags->precision > 0))
 		+ ((n && flags->alternate_form) * ft_strlen(prefix));
 	flags->width -= len;
@@ -77,9 +78,10 @@ int	print_unsigned(va_list *ap, t_flags flags, char *base, char *prefix)
 			ft_putchar_fd(flags.padding, 1);
 	if (flags.alternate_form && n)
 		ft_putstr_fd(prefix, 1);
-	if (flags.precision == 1 && !n)
-		ft_putchar_fd('0', 1);
-	else if (n || flags.precision)
+	if (flags.precision != -1 && !n)
+		while (flags.precision-- > 0)
+			ft_putchar_fd('0', 1);
+	else
 		print_ull(n, flags.precision, base, base_len);
 	while (flags.width-- > 0)
 		ft_putchar_fd(' ', 1);
