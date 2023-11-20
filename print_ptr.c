b8994fa6f7d6e860d3b01089e7fb40b859da2af6
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-20 14:14:34 +0100

    Fix put_nil: handle width

--- print_ptr.c
+++ print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:55:40 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/20 12:25:30 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/20 14:10:16 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	put_nil(t_flags flags)
 	int	len;
 
 	len = 5;
+	flags.width -= len;
 	if (!flags.left_adjust)
 	{
 		while (flags.width-- > 0)
@@ -34,5 +35,10 @@ int	put_nil(t_flags flags)
 		}
 	}
 	write(1, "(nil)", 5);
+	while (flags.width-- > 0)
+	{
+		ft_putchar_fd(' ', 1);
+		len++;
+	}
 	return (len);
 }
